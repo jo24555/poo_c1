32ab078c822c9a4a67d5a08462b5bfe06025ec13
jo24555 <jcastillol24@miumg.edu.gt>
2026-03-14 20:50:30 -0600

    tarea 3

---
+++ clase 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numero;
+
+    do{
+        cout << "Ingrese un numero: ";
+        cin >> numero;
+
+    }while(numero <= 100);
+
+    cout << "Numero mayor a 100 ingresado." << endl;
+
+    return 0;
+}
+
+
